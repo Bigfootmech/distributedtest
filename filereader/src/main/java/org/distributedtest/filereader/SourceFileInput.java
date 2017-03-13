f020711a7363ca648cbfc9dc14f2b1d6898ee800
Bigfootmech <bigfootmech@gmail.com>
2017-03-13 19:22:38 +0000

    AMEX-2 Fixing input stream to work outside eclipse

--- filereader/src/main/java/org/distributedtest/filereader/SourceFileInput.java
+++ filereader/src/main/java/org/distributedtest/filereader/SourceFileInput.java
@@ -1,9 +1,10 @@
 package org.distributedtest.filereader;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -11,9 +12,21 @@ public class SourceFileInput{
 	private static final String SOURCE_FILE = "source.txt";
 
 	public static void applyPerLine(Consumer<? super String> function) throws IOException, URISyntaxException{
-        
-		try (Stream<String> stream = Files.lines(Paths.get(ClassLoader.getSystemResource(SOURCE_FILE).toURI()))) {
+		
+		try (Stream<String> stream = asStringStream(getSourceFileInputStream())) {
 			stream.forEach(function);
 		}
+	}
+
+	private static InputStream getSourceFileInputStream() {
+		return getClassLoader().getResourceAsStream(SOURCE_FILE);
+	}
+
+	private static Stream<String> asStringStream(InputStream inputStream) {
+		return new BufferedReader(new InputStreamReader(inputStream)).lines();
+	}
+
+	private static ClassLoader getClassLoader() {
+		return SourceFileInput.class.getClassLoader();
 	}
 }
